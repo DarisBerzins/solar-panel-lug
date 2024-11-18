2c68af1b36e021228f13723733d9bd0cd3393349
Andrés Blanquer <maquinsa101abb@gmail.com>
2024-11-18 15:41:06 +0100

    FindFastenerCg function is finished!

--- bearing-failure.py
+++ bearing-failure.py
@@ -1,9 +1,25 @@
 #this is going to calculate whether bearing failure!!!
+import numpy as np
 
 print("hello world")
 
 def FindFastenerCg (FastArray):
-    # Takes an array with vectors of the form [X position, Y position, Diameter]
+    # Takes an array with vectors of the form [X position, Y position, Z position, Diameter]
     # Returns a vector of the form [CgX, CgY]
-    
-    return np.array((CgX, CgY))
+
+    # Multiply the position values times their area
+    FastArray[:, 0] = FastArray[:, 0] * ((FastArray[:, 3]) ** 2)
+    FastArray[:, 1] = FastArray[:, 1] * ((FastArray[:, 3]) ** 2)
+    FastArray[:, 2] = FastArray[:, 2] * ((FastArray[:, 3]) ** 2)
+    FastArray[:, 3] = (FastArray[:, 3] ** 2)
+
+    # Calculate the total area
+    total_area = np.sum(FastArray[:, 3])
+    print(total_area)
+
+    # Take the average
+    CgX = np.sum(FastArray[:, 0] / total_area)
+    CgY = np.sum(FastArray[:, 1] / total_area)
+    CgZ = np.sum(FastArray[:, 2] / total_area)
+
+    return np.array([CgX, CgY, CgZ])
