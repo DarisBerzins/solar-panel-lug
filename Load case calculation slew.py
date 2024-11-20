f8e807460a1bc1b8e1dd340d963a8211274ebeae
camille164 <camille.bonvoisin@gmail.com>
2024-11-20 17:00:29 +0100

    new python

--- Load case calculation slew.py
+++ Load case calculation slew.py
@@ -4,16 +4,21 @@ Isa=1 #moment of inertia of solar array
 Isc=2 #moment of inertia of spacecraft
 L_cm = 0.9573 #[m]
 #torque on the solar array
-TorqueSA=TorqueSC*(Isa)/(Isc)
+Tx=TorqueSC*np.cos(np.deg2rad(10))*np.cos(np.deg2rad(63))
+Ty=TorqueSC*np.cos(np.deg2rad(10))*np.cos(np.deg2rad(27))
+Tz=TorqueSC*np.cos(np.deg2rad(80)) #acting upwards
+
 #decomposate force in function of direction of max torque and adjust for opposite direction due to being reactions
-Tx=TorqueSA*np.cos(np.deg2rad(10))*np.cos(np.deg2rad(63))
-Ty=TorqueSA*np.cos(np.deg2rad(10))*np.cos(np.deg2rad(27))
-Tz=TorqueSA*np.cos(np.deg2rad(80)) #acting upwards
+TorqueSAx=Tx*(Isa[1])/(Isc[1])
+TorqueSAy=Ty*(Isa[2])/(Isc[2])
+TorqueSAz=Tz*(Isa[3])/(Isc[3])
+
+
 #find corresponding Force
-R_y=Tz/L_cm
-R_z=-Ty/L_cm
+R_y=TorqueSAz/L_cm
+R_z=-TorqueSAy/L_cm
 #Moments
-M_x=Tx
+M_x=TorqueSAx
 F_launch = np.array([0, R_y, R_z])
 M_launch = np.array([M_x,0,0])
 print(F_launch)
