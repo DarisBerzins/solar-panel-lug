e80ff0ff96e3ef074b55b3c45dd7e7f8cec1d145
camille164 <camille.bonvoisin@gmail.com>
2024-12-02 14:28:17 +0100

    Update Load case calculation.py

--- Load case calculation.py
+++ Load case calculation.py
@@ -4,10 +4,11 @@ lat = 2 #[g]
 lon = 3.5 #[g]
 weight= (4.515+1)*9.81 #[N]
 L_cm = 0.9573 #[m]
+Marm=1.650/2+0.9573 # distance from center of mass solar array and main body
 g=9.81 #[m/s^2]
 F_thruster=511
 #<3
-
+#load case during launch
 #sum of forces in x
 R_x=-lat*g #[N]
 #sum of moments in x
@@ -20,8 +21,20 @@ M_y=L_cm*lat*g #[N]
 R_z=-lon*g #[N]
 #sum of moments in z
 M_z=0
+
 F_launch = np.array([R_x, R_y, R_z])
 M_launch = np.array([M_x, M_y, M_z])
 
 print(F_launch)
 print(M_launch)
+
+#Load case main thruster
+#sum of forces in x
+R_xt=F_thruster
+#sum of moments around z
+M_zt=F_thruster*Marm
+
+F_thruster = np.array([R_xt, 0, 0])
+M_thruster = np.array([0, 0, M_zt])
+print(F_thruster)
+print(M_thruster)
